35bf3afce0b10e4260a911bc12d6173ab90d3a0b
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-10-14 18:12:36 -0300

    Add initial value for some-item
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- src/org/eclipse/scada/da/server/exporter/ethernetip/Rockwell.java
+++ src/org/eclipse/scada/da/server/exporter/ethernetip/Rockwell.java
@@ -1,7 +1,10 @@
 package org.eclipse.scada.da.server.exporter.ethernetip;
 
+import org.eclipse.scada.core.Variant;
 import org.eclipse.scada.da.server.browser.common.FolderCommon;
+import org.eclipse.scada.da.server.common.AttributeMode;
 import org.eclipse.scada.da.server.common.impl.HiveCommon;
+import org.eclipse.scada.utils.collection.MapBuilder;
 
 public class Rockwell extends HiveCommon {
 
@@ -28,6 +31,15 @@ public class Rockwell extends HiveCommon {
 		final RockwellDataItem item = new RockwellDataItem("some-item");
 		registerItem(item);
 		this.rootFolder.add("some-item", item, null);
+		
+		item.updateData (
+                Variant.valueOf ( 0 ),
+                new MapBuilder<String, Variant> ()
+                        .put ( "description",
+                                Variant.valueOf ( "some item" ) )
+                        .put ( "timestamp",
+                                Variant.valueOf ( System.currentTimeMillis () ) )
+                        .getMap (), AttributeMode.UPDATE );
 	}
 
 }
--- src/org/eclipse/scada/da/server/exporter/ethernetip/RockwellDataItem.java
+++ src/org/eclipse/scada/da/server/exporter/ethernetip/RockwellDataItem.java
@@ -1,12 +1,19 @@
 package org.eclipse.scada.da.server.exporter.ethernetip;
 
+import java.io.DataInputStream;
+import java.util.Map;
+
+import org.eclipse.scada.core.Variant;
+import org.eclipse.scada.da.server.common.DataItemInputCommon;
 import org.eclipse.scada.da.server.common.MemoryDataItem;
 
-public class RockwellDataItem extends MemoryDataItem {
+public class RockwellDataItem extends DataItemInputCommon {
 
 	public RockwellDataItem(String name) {
 		super(name);
 
 	}
+	
+	 
 
 }
